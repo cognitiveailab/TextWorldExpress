5c9a76398f8dd6cd2697b00d71c72c08117fa497
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2022-07-28 16:50:15 -0400

    Fixing PyPi installation issue.

--- setup.py
+++ setup.py
@@ -8,20 +8,21 @@ with open(os.path.join("textworld_express", "version.py")) as f:
 BASEPATH = os.path.dirname(os.path.abspath(__file__))
 JAR_FILE = 'textworld-express-{version}.jar'.format(version=VERSION)
 JAR_PATH = os.path.join(BASEPATH, 'textworld_express', JAR_FILE)
-OBJECTS_LUT_FILE = "object_type_ids.tsv"
+OBJECTS_TWC_FILE = "twc_objects.json.folds.json"
+COOKING_WORLD_FILE = "cooking_world.json"
 
 if not os.path.isfile(JAR_PATH):
     print('ERROR: Unable to find required library:', JAR_PATH)
     sys.exit(1)
 
-setup(name='textworld-express',
+setup(name='textworld_express',
     version=VERSION,
     description='TextWorldExpress: a highly optimized reimplementation of three text game benchmarks focusing on instruction following, commonsense reasoning, and object identification.',
     author='Peter Jansen',
     packages=['textworld_express'],
     include_package_data=True,
     package_dir={'textworld_express': 'textworld_express'},
-    package_data={'textworld_express': [JAR_FILE, OBJECTS_LUT_FILE]},
+    package_data={'textworld_express': [JAR_FILE, OBJECTS_TWC_FILE, COOKING_WORLD_FILE]},
     url="https://github.com/cognitiveailab/TextWorldExpress",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",

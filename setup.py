da6abe461ce8b06a2c414a5bf069b064a521ed99
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2022-07-29 11:36:52 -0400

    Formatting and preparing for release

--- setup.py
+++ setup.py
@@ -1,19 +1,28 @@
 import os.path, sys
+from os.path import join as pjoin
 
 from setuptools import setup
 
-with open(os.path.join("textworld_express", "version.py")) as f:
+# Check if all required files are there.
+with open(pjoin("textworld_express", "version.py")) as f:
     VERSION = f.readlines()[0].split("=")[-1].strip("' \n")
 
 BASEPATH = os.path.dirname(os.path.abspath(__file__))
 JAR_FILE = 'textworld-express-{version}.jar'.format(version=VERSION)
-JAR_PATH = os.path.join(BASEPATH, 'textworld_express', JAR_FILE)
-OBJECTS_TWC_FILE = "twc_objects.json.folds.json"
+JAR_PATH = pjoin(BASEPATH, 'textworld_express', JAR_FILE)
+OBJECTS_TWC_FILE = "twc_objects.folds.json"
+OBJECTS_TWC_PATH = pjoin(BASEPATH, 'textworld_express', OBJECTS_TWC_FILE)
 COOKING_WORLD_FILE = "cooking_world.json"
+COOKING_WORLD_PATH = pjoin(BASEPATH, 'textworld_express', COOKING_WORLD_FILE)
 
-if not os.path.isfile(JAR_PATH):
-    print('ERROR: Unable to find required library:', JAR_PATH)
-    sys.exit(1)
+missing_file = False
+for required_file in [JAR_PATH, OBJECTS_TWC_PATH, COOKING_WORLD_PATH]:
+    if not os.path.isfile(required_file):
+        print('ERROR: Unable to find required file:', required_file)
+        missing_file = True
+
+if missing_file:
+    sys.exit(1)  # Do not move forward with the installation.
 
 setup(name='textworld_express',
     version=VERSION,

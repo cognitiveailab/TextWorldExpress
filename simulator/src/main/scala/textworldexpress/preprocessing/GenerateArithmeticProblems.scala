d581b7bb82ba4a0113ef90eb101376c8a548d19a
PeterAJansen <pajansen@email.arizona.edu>
2022-08-08 10:34:56 -0700

    Working on arithmetic problem generation code / repeatability

--- simulator/src/main/scala/textworldexpress/preprocessing/GenerateArithmeticProblems.scala
+++ simulator/src/main/scala/textworldexpress/preprocessing/GenerateArithmeticProblems.scala
@@ -8,7 +8,7 @@ object GenerateArithmeticProblems {
 
   def main(args:Array[String]): Unit = {
 
-    val numUniqueProblemsToGenerate:Int = 60
+    val numUniqueProblemsToGenerate:Int = 25*3
     val maxInt = 50
     val r = new Random(2501)
 
@@ -45,10 +45,16 @@ object GenerateArithmeticProblems {
       // Display
       for (i <- 0 until existingProblems.length) {
         println (i + ":\t" + existingProblems(i).toString() + "     (result: " + existingProblems(i).generateResult().get + ")")
+        println ("\t\t" + existingProblems(i).toCodeString())
       }
       println ("")
     }
 
+    val test = new ArithmeticProblem(num1 = 9, num2 = 36, operation = "/", manualDistractors = Array(45, 27, 324, 17, 15))
+    println (test.toString())
+    println (test.toCodeString())
+    println (test.generateResult())
+    println (test.generateDistractors().mkString(", "))
 
 
   }
@@ -57,7 +63,7 @@ object GenerateArithmeticProblems {
 }
 
 
-class ArithmeticProblem(val num1:Int, val num2:Int, val operation:String) {
+class ArithmeticProblem(val num1:Int, val num2:Int, val operation:String, val manualDistractors:Array[Int] = Array.empty[Int]) {
 
   def generateText():String = {
     if (operation == "+") return "add " + num1 + " and " + num2
@@ -96,15 +102,48 @@ class ArithmeticProblem(val num1:Int, val num2:Int, val operation:String) {
   }
 
   def generateDistractors():Array[Int] = {
+    // If manual distractors have been specified, then use them.
+    if (!this.manualDistractors.isEmpty) return this.manualDistractors
+
+    // Otherwise, generate distractors.
+    val minDistractors:Int = 5
+
+    val result = this.generateResult()
+    if (result.isEmpty) return Array.empty[Int]
+
+    val unfilteredDistractors = this.generateDistractorsHelper()
+
+    // Filter (can't be the same as the answer, must be positive, etc).
+    val filteredDistractors = new ArrayBuffer[Int]
+    for (distractor <- unfilteredDistractors) {
+      if ((distractor != result.get) && (distractor > 0)) {
+        filteredDistractors.append(distractor)
+      }
+    }
+
+    // If there are not enough distractors, then generate random ones
+    while (filteredDistractors.length < minDistractors) {
+      val randDistractor = Random.nextInt(50-1)+1
+      if ((randDistractor != result.get) && (!filteredDistractors.contains(randDistractor))) {
+        filteredDistractors.append(randDistractor)
+      }
+    }
+
+    return filteredDistractors.toArray
+  }
+
+  private def generateDistractorsHelper():Array[Int] = {
     // True is addition
     if (operation == "+") return Array(num1-num2, num2-num1, num1*num2, math.round(num2/num1).toInt, math.round(num1/num2).toInt )    // True: num1+num2
     if (operation == "-") return Array(num1+num2, num2-num1, num1*num2, math.round(num2/num1).toInt, math.round(num1/num2).toInt )    // True: num1-num2
     if (operation == "*") return Array(num1+num2, num1-num2, num2-num1, math.round(num2/num1).toInt, math.round(num1/num2).toInt )    // True: num1*num2
-    if (operation == "/") return Array(num1+num2, num1-num2, num2-num1, num1*num2, math.round(num2/num1).toInt )                      // True: num1/num2
+    if (operation == "/") return Array(num1+num2, num1-num2, num2-num1, num1*num2, math.round(num1/num2).toInt )                      // True: num1/num2
 
     // If we reach here, unknown operation
     throw new RuntimeException("ERROR: Unknown operation (" + operation + ")")
   }
+
+
 
   /*
    * Operators
@@ -143,6 +182,10 @@ class ArithmeticProblem(val num1:Int, val num2:Int, val operation:String) {
    * String methods
    */
   override def toString() = this.generateText()
+
+  def toCodeString():String = {
+    return "new ArithmeticProblem(num1 = " + num1 + ", num2 = " + num2 + ", operation = \"" + operation + "\", manualDistractors = Array(" + this.generateDistractors().mkString(", ") + "))"
+  }
 
 }
 
